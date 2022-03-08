877aea3fbb398095747ebcb9dabc76b7fa07607f
CHAODENG <90479627+CHAODENG@users.noreply.github.com>
2022-03-07 19:47:05 -0500

    Add files via upload

---
+++ Web application/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='PatientMonitorSystem',
+    version='3.9.7',
+    packages=['package'],
+    url='',
+    license='',
+    description='PatientMonitorSystem'
+)
